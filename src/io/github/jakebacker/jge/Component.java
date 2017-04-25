75d84c92826442e3db9e90444146df9fd5f6eae4
Jake Backer <jbacker42@gmail.com>
2017-04-25 15:26:58 -0400

    Hacks... Hacks everywhere

--- src/io/github/jakebacker/jge/Component.java
+++ src/io/github/jakebacker/jge/Component.java
@@ -1,4 +1,5 @@
 package io.github.jakebacker.jge;
 
 public class Component {
+
 }
--- src/io/github/jakebacker/jge/GameObject.java
+++ src/io/github/jakebacker/jge/GameObject.java
@@ -1,5 +1,16 @@
 package io.github.jakebacker.jge;
 
-public class GameObject {
+import java.util.ArrayList;
 
+public class GameObject {
+	private ArrayList<Component> components = new ArrayList<>();
+
+	public <T> Component getComponent(Class<T> clazz) {
+
+		for(Component c : components) {
+			if (c.getClass().getName().equals(clazz.getName())) { // HACK
+
+			}
+		}
+	}
 }
